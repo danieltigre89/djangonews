df10597c9aab4c352ff61974e0227a99dc3761d6
damarilla <lamarilla@copaco.com.py>
2026-06-03 12:40:29 +0000

    custom user model

--- django_news/settings.py
+++ django_news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-blwv-gofwrtw259zq7&=!j*l8f*o_q#^jlu^^k4=tp6=zu116x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
